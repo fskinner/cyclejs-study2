283dd5c6be5afbce921f8f6a36eb6d6d9c74504f
Felipe Skinner <felipeskinner@gmail.com>
2016-05-09 00:20:35 -0300

    Refactor todo model file

--- src/dialogue/pages/todos/todos-model.js
+++ src/dialogue/pages/todos/todos-model.js
@@ -1,90 +1,23 @@
 import { Observable } from 'rx';
+import Operations from './todo-operations';
 
 const initialState = {
   items: [{ id: "0", text: 'Study', completed: false, editing: false }],
   archive: []
 };
 
-const idIncrement = (array) => {
-  if(array.length === 0) {
-    return 0;
-  }
-
-  return array[array.length - 1].id + 1
-};
-
-const Operations = {
-  Add: todoText => state => ({
-    items: [ ...state.items,
-      {
-        id: idIncrement(state.items).toString(),
-        text: todoText,
-        completed: false
-      }
-    ],
-    archive: state.archive
-  }),
-
-  Remove: todoId => state => ({
-    items: state.items.filter(x => x.id !== todoId),
-    archive: state.archive
-  }),
-
-  Mark: (todoId, status) => state => ({
-    items: state.items.map(x => {
-      if(x.id === todoId) {
-        x.completed = status;
-      }
-      return x;
-    }),
-    archive: state.archive
-  }),
-
-  Archive: completeStatus => state => ({
-    items: state.items.filter(x => x.completed !== completeStatus),
-    archive: state.archive.concat(state.items.filter(x => x.completed === completeStatus))
-  }),
-
-  ToggleEdit: (todoId, status) => state => ({
-    items: state.items.map(x => {
-      if(x.id === todoId) {
-        x.editing = status;
-      }
-      return x;
-    }),
-    archive: state.archive
-  }),
-
-  SaveEdit: ({id, text}) => state => ({
-    items: state.items.map(x => {
-      if(x.id === id) {
-        x.text = text;
-        x.editing = false;
-      }
-      return x;
-    }),
-    archive: state.archive
-  })
-};
-
 const todosModel = (actions) => {
-  const {
-    addTodo$, removeTodo$, markTodo$,
-    unmarkTodo$, archiveComplete$,
-    toggleEdit$, cancelEdit$, editTodo$
-  } = actions;
+  const addOp$ = actions.addTodo$.map(todoText => Operations.Add(todoText));
+  const removeOp$ = actions.removeTodo$.map(todoId => Operations.Remove(todoId));
 
-  const addOp$ = addTodo$.map(todoText => Operations.Add(todoText));
-  const removeOp$ = removeTodo$.map(todoId => Operations.Remove(todoId));
+  const markOp$ = actions.markTodo$.map(todoId => Operations.Mark(todoId, true));
+  const unmarkOp$ = actions.unmarkTodo$.map(todoId => Operations.Mark(todoId, false));
 
-  const markOp$ = markTodo$.map(todoId => Operations.Mark(todoId, true));
-  const unmarkOp$ = unmarkTodo$.map(todoId => Operations.Mark(todoId, false));
+  const archiveCompleteOp$ = actions.archiveComplete$.map( _ => Operations.Archive(true));
 
-  const archiveCompleteOp$ = archiveComplete$.map( _ => Operations.Archive(true));
-
-  const toggleEditOp$ = toggleEdit$.map(todoId => Operations.ToggleEdit(todoId, true));
-  const cancelEditOp$ = cancelEdit$.map(todoId => Operations.ToggleEdit(todoId, false));
-  const saveEditOp$ = editTodo$.map(todo => Operations.SaveEdit({...todo}));
+  const toggleEditOp$ = actions.toggleEdit$.map(todoId => Operations.ToggleEdit(todoId, true));
+  const cancelEditOp$ = actions.cancelEdit$.map(todoId => Operations.ToggleEdit(todoId, false));
+  const saveEditOp$ = actions.editTodo$.map(todo => Operations.SaveEdit({...todo}));
 
   const allOperations$ = Observable.merge(
     addOp$, removeOp$, markOp$, unmarkOp$,
